e45cccc166bf78239d46946b1ff83ec8d80fb687
nazar1000 <nazar_feshchyn@interia.pl>
2021-09-24 17:05:21 +0100

    Initial commit

---
+++ ajax.js
@@ -0,0 +1,24 @@
+//for database support only ignore...
+
+function sendData() {
+    if (true) return; //remove this for database
+    let FoundResults = new Promise(function (myResolve, myReject) {
+        fetch("db.php", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+            },
+            body: new FormData(document.getElementById("canvas_form")),
+        })
+
+            //Displays error messages or something
+            // .then((response) => response.json())
+            .then((text) => {
+                myResolve(text);
+            });
+    });
+
+    // FoundResults.then(function (text) {
+    //     // console.log(text)
+    // })
+}
---
+++ db.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+$global = array();
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "127.0.0.1";
+$user = "root";
+$pass = "";
+$database_name = "camera_cctv";
+
+$conn = mysqli_connect($servername, $user, $pass, $database_name, 3306);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+
+// print_r($_POST);
+
+if ($_POST["date"]) {
+    // $_SESSION["testing"] = "not working";
+
+    // sending data
+    $_date = $_POST["date"];
+    $_time = $_POST["time"];
+    $_color = $_POST["color"];
+
+    $query = "INSERT INTO cam_triggers (date, time, color) 
+    VALUES ('$_date', '$_time', '$_color');";
+    echo $query;
+    //getting data for send data
+    $result = mysqli_query($conn, $query);
+
+    //Getting data for request
+
+    // $query1 = "SELECT * FROM cam_triggers;";
+    // $resultOut = mysqli_query($conn, $query1);
+    // // $checked = $resultOut ? "true" : "false";
+    // // echo $checked;
+    // $resultCheckOut = mysqli_num_rows($resultOut); //returns arrays of row
+    // // print_r($resultCheckOut);
+    // if ($resultCheckOut > 0) { //if rows* are more then 0....
+    //     while ($row = mysqli_fetch_assoc($resultOut)) { //converts the output to named arrays based on row name
+
+    //         $dBData = array($row["trigger_id"], $row["date"], $row["time"], $row["color"]);
+    //         array_push($global, $dBData);
+    //     }
+    // } else {
+
+    //     array_push($global, "Error: " . mysqli_error($conn));
+    // }
+    mysqli_close($conn);
+
+
+    // $myJSON = json_encode($global);
+    // echo $myJSON;
+    // die();
+
+    $_SESSION["db-return"] = $global;
+    // header('Location: '. "http://127.0.0.1/server/Database%20Trial");
+    die();
+}
